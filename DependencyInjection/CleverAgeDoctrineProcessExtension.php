eeb6393329959b412cc0fdf9c4cf8225c33aaa50
VincentChalnot <vincent.chalnot@gmail.com>
2021-05-31 16:13:22 +0200

    Removing sidus/base-bundle dependency

--- DependencyInjection/CleverAgeDoctrineProcessExtension.php
+++ DependencyInjection/CleverAgeDoctrineProcessExtension.php
@@ -10,17 +10,30 @@
 
 namespace CleverAge\DoctrineProcessBundle\DependencyInjection;
 
-use Sidus\BaseBundle\DependencyInjection\SidusBaseExtension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
- * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * @see    http://symfony.com/doc/current/cookbook/bundles/extension.html
  *
  * @author Valentin Clavreul <vclavreul@clever-age.com>
  * @author Vincent Chalnot <vchalnot@clever-age.com>
  * @author Madeline Veyrenc <mveyrenc@clever-age.com>
  */
-class CleverAgeDoctrineProcessExtension extends SidusBaseExtension
+class CleverAgeDoctrineProcessExtension extends Extension
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
+    }
 }
