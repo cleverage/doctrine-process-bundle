1f75c8f1541d6685bdbe399b6ca05285cb3f2626
Xavier Marchegay <xavier.marchegay@gmail.com>
2023-10-09 21:13:05 +0200

    fix CleverAgeDoctrineProcessBundle.php location

--- src/CleverAgeDoctrineProcessBundle.php
+++ src/CleverAgeDoctrineProcessBundle.php
@@ -1,14 +1,17 @@
-<?php declare(strict_types=1);
-/**
- * This file is part of the CleverAge/DoctrineProcessBundle package.
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the CleverAge/ProcessBundle package.
  *
- * Copyright (C) 2017-2023 Clever-Age
+ * Copyright (c) 2017-2023 Clever-Age
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
 
-namespace CleverAge\DoctrineProcessBundle;
+namespace CleverAge\ProcessBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
